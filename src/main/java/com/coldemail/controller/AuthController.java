eece3712eeea01caaea4ca3ca300378f7c8cfe8a
saloniranka03 <saloniranka0303@gmail.com>
2025-09-27 00:45:27 -0700

    Update AuthController.java

--- src/main/java/com/coldemail/controller/AuthController.java
+++ src/main/java/com/coldemail/controller/AuthController.java
@@ -20,7 +20,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = {"http://localhost:3000", "https://cold-email-app.netlify.app"})
+@CrossOrigin(origins = {"http://localhost:3000", "https://cold-email-app.netlify.app"}, allowCredentials = "true"))
 public class AuthController {
 
     @Value("${app.frontend.url}")
