51e704f942495fba9ff0f617d222d4d1c0b6e502
saloniranka03 <saloniranka0303@gmail.com>
2025-09-27 00:20:15 -0700

    Update AuthController.java - javax to jakarta

--- src/main/java/com/coldemail/controller/AuthController.java
+++ src/main/java/com/coldemail/controller/AuthController.java
@@ -7,9 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
